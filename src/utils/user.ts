e41b5edd9bfd06ef2e428239d8c3ceab61153339
mxrain <mxrain@yeah.net>
2024-05-03 22:57:36 +0800

    add

---
+++ src/utils/user.ts
@@ -0,0 +1,17 @@
+// @ts-nocheck
+// Copyright @ 2018-present xiejiahe. All rights reserved. MIT license.
+// See https://github.com/mxrain/nav
+
+export function getToken() {
+  return window.localStorage.getItem('token')
+}
+
+export function setToken(token: string) {
+  return window.localStorage.setItem('token', token)
+}
+
+export function removeToken() {
+  return window.localStorage.removeItem('token')
+}
+
+export const isLogin: boolean = !!getToken()
